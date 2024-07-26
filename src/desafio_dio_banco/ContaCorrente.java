e53534d52bb6d431e3cd9b78ca5b535a42e3a3d0
Henrique <henrique.c.lucas@hotmail.com>
2024-07-26 14:00:58 -0300

    Adicionando

---
+++ src/desafio_dio_banco/ContaCorrente.java
@@ -0,0 +1,19 @@
+package desafio_dio_banco;
+
+public class ContaCorrente extends Conta {
+
+    @Override
+    public void imprimirExtrato() {
+	System.out.println("Extrato Conta Corrente: ");
+	super.imprimirInformacoes();
+
+
+    }
+
+   
+    
+    
+    
+}
+
+
---
+++ src/desafio_dio_banco/ContaPoupanca.java
@@ -0,0 +1,16 @@
+package desafio_dio_banco;
+
+public class ContaPoupanca extends Conta {
+
+    @Override
+    public void imprimirExtrato() {
+	System.out.println("Extrato Conta Poupança: ");
+	super.imprimirInformacoes();
+	
+    }
+
+    
+
+
+  
+}
